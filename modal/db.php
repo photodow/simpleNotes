ae2f6059bac80736ff84abcd879b3e070b918b82
James <photodow@gmail.com>
2014-01-17 21:41:01 -0500

    setCred() functionality

--- modal/db.php
+++ modal/db.php
@@ -8,9 +8,13 @@
 		private $db;
 
 		public function __construct($dbName){
+			$this->setCred($dbName, 'photodow', '67QceLYPWwyHZTET', 'localhost', '3306');
 		}
 
 		private function setCred($dbName, $user, $pass, $host, $port){
+			$this->dsn = 'mysql:host=' . $host . ';port=' . $port .';dbname=' . $dbName;
+			$this->user = $user;
+			$this->pass = $pass;
 		}
 
 		private function connectDB(){
