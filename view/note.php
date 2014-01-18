34f32540bc896f050484e7424342f521adcd8cf8
James <photodow@gmail.com>
2014-01-17 23:13:00 -0500

    view notes view

---
+++ view/note.php
@@ -0,0 +1,24 @@
+<?php
+
+	$id = $_GET['id'];
+
+	if(is_numeric($id)){
+		include_once 'modal/notes.php';
+
+		$notesModal = new SingleNoteModal($_GET[id]);
+		$data = $notesModal->getData();
+
+		if($data){
+
+			echo '<h1>' . $data['title'] . '</h1>';
+			echo '<p>' . $data['noteText'] . '</p>';
+			echo '<a href="/simpleNotes/">Go Back</a>';
+
+		}else{
+			header('Location: /simpleNotes/');
+		}
+	}else{
+		header('Location: /simpleNotes/');
+	}
+
+?>
