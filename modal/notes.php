4249d4a87d32efa1603f8fd3ad2290eb16877d97
James <photodow@gmail.com>
2014-01-17 23:12:04 -0500

    copied notesModal and created singlenotemodal by id

--- modal/notes.php
+++ modal/notes.php
@@ -1,5 +1,7 @@
 <?php
 
+	include_once 'modal/db.php';
+
 	class NotesModal {
 
 		private $data;
@@ -21,6 +23,31 @@
 
 		public function getData(){
 			return $this->data;
+		}
+
+	}
+
+	class SingleNoteModal {
+
+		private $data;
+
+		public function __construct($id){
+			$this->selectData($id);
+		}
+
+		private function selectData($id){
+
+			$dbModel = new dbModel('dbf1401');
+			$dbRef = $dbModel->getDB();
+			$query = $dbRef->prepare("SELECT * FROM notes WHERE noteStatus = 'public' AND id = " . $id);
+			$query->execute();
+
+			$this->data = $query->fetchAll();
+
+		}
+
+		public function getData(){
+			return $this->data[0];
 		}
 
 	}
