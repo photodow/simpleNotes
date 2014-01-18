0fa851373882a2982f57f326aaa85ef2f18935d8
James <photodow@gmail.com>
2014-01-17 21:39:52 -0500

    added the methods for this class

--- modal/db.php
+++ modal/db.php
@@ -1,11 +1,23 @@
 <?php
 
 	class dbModel {
-		
+
 		private $dsn;
 		private $user;
 		private $pass;
 		private $db;
+
+		public function __construct($dbName){
+		}
+
+		private function setCred($dbName, $user, $pass, $host, $port){
+		}
+
+		private function connectDB(){
+		}
+
+		public function getDB(){
+		}
 
 	}
 
