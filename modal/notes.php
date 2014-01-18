a11b9e5ba14dcaa34659fff24494f3ac413fb952
James <photodow@gmail.com>
2014-01-17 22:42:50 -0500

    select data functionality

--- modal/notes.php
+++ modal/notes.php
@@ -5,10 +5,17 @@
 		private $data;
 
 		public function __construct(){
-
+			$this->selectData();
 		}
 
 		private function selectData(){
+
+			$dbModel = new dbModel('dbf1401');
+			$dbRef = $dbModel->getDB();
+			$query = $dbRef->prepare("SELECT * FROM notes WHERE noteStatus = 'public'");
+			$query->execute();
+
+			$this->data = $query->fetchAll();
 
 		}
 
