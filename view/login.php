1fc2e6e6a12aba106fe62e9e85511500aa76bb23
James <photodow@gmail.com>
2014-01-20 22:32:15 -0500

    added a login form

---
+++ view/login.php
@@ -0,0 +1,15 @@
+<p>Pleast login to Create, Read, Update and Delete your notes.</p>
+
+<form method="post" action="?">
+	<div>
+		<label for="username">Username</label>
+		<input type="text" name="username" id="username" />
+	</div>
+	<div>
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" />
+	</div>
+	<div>
+		<input type="submit" id="submit" value="Login" />
+	</div>
+</form>
