0f5c1f7d691aee31f8e23083ea7c5c5e257715e8
James <photodow@gmail.com>
2014-01-20 22:31:48 -0500

    added navigation to the footer

--- view/footer.php
+++ view/footer.php
@@ -5,10 +5,13 @@
 					<a href="/simpleNotes/" title="Simple Notes Home">Home</a>
 				</li>
 				<li>
-					<a href="/simpleNotes/?listview" title="List of Simple Notes">List Notes</a>
+					<a href="/simpleNotes/?loggedin&amp;listview" title="List of Simple Notes">List Notes</a>
 				</li>
 				<li>
 					<a href="/simpleNotes/?logout" title="Logout of Simple Notes">Logout</a>
+				</li>
+				<li>
+					<a href="/simpleNotes/?login" title="Login to Simple Notes">Login</a>
 				</li>
 			</ul>
 		</footer>
