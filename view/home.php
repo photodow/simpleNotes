d802be9246e0e5b866b58c4d7123154303d55422
James <photodow@gmail.com>
2014-01-21 23:47:33 -0500

    removed unnecessary login button

--- view/home.php
+++ view/home.php
@@ -1,7 +1,3 @@
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet consectetur enim et eleifend. Suspendisse cursus est vel hendrerit iaculis. Phasellus nibh risus, aliquam eget volutpat ac, molestie in diam. Quisque placerat suscipit sapien, vitae fermentum tellus. Pellentesque ante diam, congue eu justo vitae, ultricies tincidunt neque.</p>
 
 <p>Suspendisse id arcu elit. Vestibulum tincidunt convallis eros a porttitor. Sed convallis tellus vel sapien pellentesque interdum. Integer volutpat elit vitae diam pretium congue. Suspendisse sed nunc fringilla, porta mauris a, ultricies massa.</p>
-
-<?php if(!$sessionStatus){ ?>
-<a href="/simpleNotes/?login">Login</a>
-<?php } ?>
