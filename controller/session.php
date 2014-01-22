825ab8dbe2a80e623524c5b04c46857abdd8599f
James <photodow@gmail.com>
2014-01-21 21:40:09 -0500

    starting to build my sanitize functions

--- controller/session.php
+++ controller/session.php
@@ -3,31 +3,49 @@
 	class SessionController {
 
 		private $status;
-		private $username;
-		private $password;
 
 		public function __construct(){
 			
 		}
 
-		private function handle(){
-
-		}
-
 		private function sanitizeUser($user){
 
+			$passResults = false;
+
+			if(){
+				$passResults = 
+			}
+
+			return $passResults;
 		}
 
-		private function sanitizePass($password){
+		private function sanitizePass($pass){
 
+			$passResults = false;
+
+			return $passResults;
 		}
 
 		private function setStatus(){
 
-			if(!empty($_POST['username']) && !empty($_POST['password'])){
+			if(isset($_GET['login']) && $_GET['login'] === 'validateUser'){ // check if we are on the correct page
+
+				if(!empty($_POST['username']) && !empty($_POST['password'])){ // check if the correct information has been sent
+
+					if(sanitizeUser($_POST['username']) && sanitizePass($_POST['password'])){ // check if the information is clean
+
+						echo 'log them in';
+
+					}else{ // error redirect and inform the user
+						header('Location: /simpleNotes/?login=error');
+					}
+
+				}else{ // error redirect and inform the user
+					header('Location: /simpleNotes/?login=error');
+				}
 
 			}
-			
+
 		}
 
 		public function getStatus(){
