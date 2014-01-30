6f25819ce5b369842c2da89c3e8f3e494b31aa11
James <photodow@gmail.com>
2014-01-29 23:29:24 -0500

    added the showNote()

--- laravel/app/controllers/Pages.php
+++ laravel/app/controllers/Pages.php
@@ -3,14 +3,22 @@
 	class Pages extends BaseController {
 
 		public function showHome(){
-
 			return View::make('templates.pages', array('pageTitle' => 'Simple Notes'))->nest('navigationView', 'navigation')->nest('contentView', 'home');
 		
 		}
 
 		public function showListNotes(){
 
-			return View::make('templates.pages', array('pageTitle' => 'Simple List Notes'))->nest('navigationView', 'navigation')->nest('contentView', 'listnotes');
+			$listOfNotes = ListNotes::all();
+			return View::make('templates.pages', array('pageTitle' => 'Simple List Notes'))->nest('navigationView', 'navigation')->nest('contentView', 'listnotes', array('data' => $listOfNotes));
+		
+		}
+
+		public function showNote(){
+
+			$note = ListNotes::find($_GET['id']);
+			return View::make('templates.pages', array('pageTitle' => 'Simple Note'))->nest('navigationView', 'navigation')->nest('contentView', 'note', array('data' => $note));
+		
 		}
 
 	}
