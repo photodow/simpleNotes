881cf75b3fa83312efd1e256f059a95b9f6875f0
James <photodow@gmail.com>
2014-01-29 22:48:33 -0500

    fixed an indent issue

--- view/header.php
+++ view/header.php
@@ -8,38 +8,38 @@
 		<link type='text/css' rel='stylesheet' href='css/style.css' />
 	</head>
 	<body>
-				<aside>
-					<nav>
+		<aside>
+			<nav>
+				<ul>
+					<li>
+						<span title="Navigation"><i class="fa fa-bars"></i></span>
 						<ul>
 							<li>
-								<span title="Navigation"><i class="fa fa-bars"></i></span>
-								<ul>
-									<li>
-										<a href="/simpleNotes/" title="Simple Notes Home"><i class="fa fa-home"></i> Home</a>
-									</li>
-								<?php if($sessionStatus){ ?>
-									<li>
-										<a href="/simpleNotes/?listnotes" title="View Your Simple Notes"><i class="fa fa-list"></i> Notes</a>
-									</li>
-									<li>
-										<a href="/simpleNotes/?addNote" title="Add a Simple Note"><i class="fa fa-file-o"></i> Add Note</a>
-									</li>
-									<?php /*<li>
-										<a href="/simpleNotes/?settings" title="View/Edit Your Settings"><i class="fa fa-cog"></i> Settings</a>
-									</li>*/ ?>
-									<li>
-										<a href="/simpleNotes/?logout" title="Sign Out of Simple Notes"><i class="fa fa-sign-out"></i> Sign Out</a>
-									</li>
-								<?php }else{ ?>
-									<li>
-										<a href="/simpleNotes/?login" title="Sign In to Simple Notes"><i class="fa fa-sign-in"></i> Sign In</a>
-									</li>
-								<?php } ?>
-								</ul>
+								<a href="/simpleNotes/" title="Simple Notes Home"><i class="fa fa-home"></i> Home</a>
 							</li>
+						<?php if($sessionStatus){ ?>
+							<li>
+								<a href="/simpleNotes/?listnotes" title="View Your Simple Notes"><i class="fa fa-list"></i> Notes</a>
+							</li>
+							<li>
+								<a href="/simpleNotes/?addNote" title="Add a Simple Note"><i class="fa fa-file-o"></i> Add Note</a>
+							</li>
+							<?php /*<li>
+								<a href="/simpleNotes/?settings" title="View/Edit Your Settings"><i class="fa fa-cog"></i> Settings</a>
+							</li>*/ ?>
+							<li>
+								<a href="/simpleNotes/?logout" title="Sign Out of Simple Notes"><i class="fa fa-sign-out"></i> Sign Out</a>
+							</li>
+						<?php }else{ ?>
+							<li>
+								<a href="/simpleNotes/?login" title="Sign In to Simple Notes"><i class="fa fa-sign-in"></i> Sign In</a>
+							</li>
+						<?php } ?>
 						</ul>
-					</nav>
-				</aside>
+					</li>
+				</ul>
+			</nav>
+		</aside>
 		<section id="content">
 			<header>
 				<h1>Simple Notes</h1>
