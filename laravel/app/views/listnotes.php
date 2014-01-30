1dc903b7a96fdbe66451c346d91c0daec1262f00
James <photodow@gmail.com>
2014-01-29 23:28:59 -0500

    uncommented out to make way for the data

--- laravel/app/views/listnotes.php
+++ laravel/app/views/listnotes.php
@@ -1,12 +1,7 @@
 <ul id="notesList">
-<?php /*foreach($data as $row){ ?>
+<?php foreach($data as $row){ ?>
 	<li>
-		<ul>
-			<li><a href="?editNote=<?php echo $row['id']; ?>">&nbsp;<i class="fa fa-pencil"></i>&nbsp;</a></li>
-			<li><a href="?delete=<?php echo $row['id']; ?>">&nbsp;<i class="fa fa-trash-o"></i>&nbsp;</a></li>
-		</ul>
-		<a href="?note=<?php echo $row['id']; ?>"><?php echo $row['title']; ?></a>
+		<a href="/simpleNotes/laravel/public/listnotes/note?id=<?php echo $row['id']; ?>"><?php echo $row['title']; ?></a>
 	</li>
-<?php }*/ ?>
-	<li>testing</li>
+<?php } ?>
 </ul>
