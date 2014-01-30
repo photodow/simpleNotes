b55994bf067d4bcaeb6306d1d0ccc96a6322ffe7
James <photodow@gmail.com>
2014-01-29 22:47:15 -0500

    added navigation view

---
+++ laravel/app/views/navigation.php
@@ -0,0 +1,16 @@
+
+<nav>
+	<ul>
+		<li>
+			<span title="Navigation"><i class="fa fa-bars"></i></span>
+			<ul>
+				<li>
+					<a href="/simpleNotes/laravel/public" title="Simple Notes Home"><i class="fa fa-home"></i> Home</a>
+				</li>
+				<li>
+					<a href="/simpleNotes/laravel/public/listnotes" title="View Your Simple Notes"><i class="fa fa-list"></i> Notes</a>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</nav>
